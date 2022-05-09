60804b82ebde9986a4b748da8b7dfda42c502e86
TrvMarco <trevisanello.marco@gmail.com>
2022-05-09 14:45:53 +0200

    aggiunto analisi a step del programma

---
+++ script.js
@@ -0,0 +1,18 @@
+/*
+Il computer deve generare 16 numeri casuali tra 1 e 100.
+I numeri non possono essere duplicati.
+In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+L’utente non può inserire più volte lo stesso numero.
+Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+*/
+
+// 1. Creare funzione per generare numeri casuali da 1 a 100
+// 2. Eseguire il controllo che i numeri non siano duplicati 
+// 3. Chiedo all'utente di inserire (100-16) volte un numero tra 1 e 100
+// 4. Controllo che i numeri inseriti dall'utente non si ripetono 
+// 5. SE il numero è nella lista generata dal programma termina la partita
+//    ALTRIMENTI continuo a chiedere al giocatore di inserire altri numeri
+// LA PARTITA TERMINA QUANDO IL GIOCATORE METTE UN NUMERO SCELTO DALLA CPU O DAL MASSIMO DELLE VOLTE POSSIBILI PER INSERIRE I NUMERI
+// 6. Comunico il punteggio, ovvero il numero di volte che è stato inserito un numero consentito
