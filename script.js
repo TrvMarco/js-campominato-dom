4b1a565f27bb5451debcfa55015bc87949c3ef7e
TrvMarco <trevisanello.marco@gmail.com>
2022-05-10 14:25:35 +0200

    aggiunto controllo su numero inserito utente

--- script.js
+++ script.js
@@ -38,14 +38,18 @@ while(cpuNumbers.length < 16){  //16
 }
 console.log(cpuNumbers);
 
-const userNumbersList = []
-let i = 0
+const userNumbersList = [];
+let i = 0;
 
-let brokenNumber = false
+let brokenNumber = false;
 
-while(i < 84 && brokenNumber == false){  //84
+while(i < 5 && brokenNumber == false){  //84
     i++ 
-    const userNumbers = Number(prompt("Inserisci un numero da 1 a 100"));
+    let userNumbers;
+    do {
+        userNumbers = Number(prompt("Inserisci un numero da 1 a 100"));
+    }while(isNaN(userNumbers) || userNumbers < 1 || userNumbers > 100)
+
     if(userNumbers >= 1 && userNumbers <= 100 && !userNumbersList.includes(userNumbers)){
         for (let j = 0; j < cpuNumbers.length; j++){
             if(userNumbers === cpuNumbers[j]){
@@ -79,3 +83,28 @@ if(difficult == "facile"){
     getRndInteger(1,50);
     maxValue = 50;
 }
+
+
+
+/* CORREZIONE:
+
+FUNZIONI: 
+Math random min max
+
+
+MAIN: 
+- Array "bombs" vuoto 
+- Ciclo while finche array bombs < 16 usando funzione, controllo che non sia inserito con un if e pusho 
+- Ciclo while (prima creo variabile per "bomba esplosa" = false), condizioni ciclo: finchè bomba expl è falsa entro nel ciclo,  e numero array utente è minore di 84
+    faccio prompt number di inserire numero  (per verifica dentro un do while con isnan number || numero <1 || usernumber  >100 )
+    SE array bombs contiene il numero che ha inserito l'utente cambio valore di bomba esplosa e faccio terminare il gioco 
+    ALTRIMENTI SE l'utente ha gia messo il numero glie lo comunico con un alert 
+    ALTRIMENTI pusho array(da creare) dei numeri inserriti da utente
+- SE bomba è esplosa alert con hai perso il tuo punteggio è {arrray.lengh}
+  ALTRIMENTI hai vinto 
+
+
+BONUS: 
+costante level con numberprompt da 0 a 2 dentro un do, con while per validare che ci sia dentro isnan level, e se level < 0 || level > 2 
+SE level === 0 cambia variabile numeri totali 
+ALTRIMENTI SE ===1 etc */ 
